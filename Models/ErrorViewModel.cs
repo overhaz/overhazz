9ed7f3e9beaf362e0acc54c4039dc2bdc5a3c224
overhaz <sonok_thp@outlook.es>
2018-09-22 12:08:03 -0500

    Proyecto inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace overhazz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
